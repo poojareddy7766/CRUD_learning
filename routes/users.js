1d7f3c8329385c50cc23d4bbdf8b252248522b41
Pooja M <pooja.m@alteryx.com>
2024-12-10 20:16:48 +0530

    create a post request

--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,42 @@
-// import express module
+//  GET Request
+
+// // import express module
+// import express from 'express';
+// //creates a router instance
+// const router = express.Router();
+// //mock data
+// const users = [
+//     {
+//       firstName: 'Pooja',
+//       lastName: 'reddy',
+//       email: 'poojareddy@gmail.com',
+//     },
+//     {
+//       firstName: 'John',
+//       lastName: 'Cena',
+//       email: 'JonCena@example.com',
+//     },
+//   ];
+// // get it from the same file and send 
+//   router.get('/', (req, res) => {
+//     res.send(users);
+// })
+// // export it
+// export default router
+
+// POST Request
+
+
 import express from 'express';
-//creates a router instance
+import { v4 as uuidv4 } from 'uuid';
 const router = express.Router();
-//mock data
-const users = [
-    {
-      firstName: 'Pooja',
-      lastName: 'reddy',
-      email: 'poojareddy@gmail.com',
-    },
-    {
-      firstName: 'John',
-      lastName: 'Cena',
-      email: 'JonCena@example.com',
-    },
-  ];
-// get it from the same file and send 
-  router.get('/', (req, res) => {
-    res.send(users);
-})
-// export it
+const users = [];
+router.post('/', (req, res) => {
+  const user = req.body;
+
+  users.push({ ...user, id: uuidv4() });
+  console.log(users);
+
+  res.send(`${user.firstName} added to the DB`);
+})  
 export default router
