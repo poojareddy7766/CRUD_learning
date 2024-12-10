c7c67b3b3ddcf18a7330f5f497a93d4578f0dd29
Pooja M <pooja.m@alteryx.com>
2024-12-10 13:12:07 +0530

    testing api get request

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ import bodyParser from 'body-parser'
 // create app using express object
 const app = express();
 //define port 
+// import userRoutes from get endpoint
+import userRoutes from './routes/users.js'
 const PORT = 3000
 // parsing the body to json
 app.use(bodyParser.json());
 // listening to the port
-
+app.use('/users', userRoutes);
 // directing to the same file , sending the response
 app.get('/', (req, res) => {
     console.log('[GET ROUTE]');
